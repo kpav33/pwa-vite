f85f283cb89ad93e200e288c175204f77b6c41ea
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-08-22 10:56:28 +0200

    Add updated assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
+import { useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +8,17 @@ import './App.css'
 // yarn create vite my-pwa --template react
 // yarn add vite-plugin-pwa -D
 
+// https://vite-pwa-org.netlify.app/deployment/netlify.html
+// You don't need to follow this deployment guide, everything seems to work ok if you just deploy directly with Netlify without any adjustments
+
+// https://github.com/vite-pwa/vite-plugin-pwa/tree/main/examples/react-router Official Vite PWA with React Router example
+
+// https://vite-pwa-org.netlify.app/guide/, https://css-tricks.com/vitepwa-plugin-offline-service-worker/ => Useful PWA explanations
+
+// Use @vite-pwa/assets-generator to generate the necessary image assets check the https://vite-pwa-org.netlify.app/assets-generator/ to see how it works
+
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +29,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
       </div>
       <div>
         <h1>Hello PWA app!</h1>
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     react(),
     VitePWA({
       // Manifest generator: https://www.simicart.com/manifest-generator.html/
-      // Create icons: https://realfavicongenerator.net/ or https://tools.crawlink.com/tools/pwa-icon-generator/
+      // Create icons: https://realfavicongenerator.net/ or https://tools.crawlink.com/tools/pwa-icon-generator/ or pwa-asset-generator npm package
+      // Best option to generate assets for Vite PWA seems to be to just use the official Vite one => @vite-pwa/assets-generator npm package
       // add this to cache all the imports
       // Without this the images will not be cached, if you want for everything to be cached and be fully available offline you need to add the workbox and includeAssets properties
       // If you dont want to cache everything you can adjust the globs accordingly
@@ -28,25 +29,48 @@ export default defineConfig({
         description: "testing vite pwa",
         name: "vite test",
         icons: [
+          // {
+          //   src: "/icon-192x192.png",
+          //   sizes: "192x192",
+          //   type: "image/png",
+          // },
+          // // {
+          // //   src: "/icon-256x256.png",
+          // //   sizes: "256x256",
+          // //   type: "image/png",
+          // // },
+          // {
+          //   src: "/icon-384x384.png",
+          //   sizes: "384x384",
+          //   type: "image/png",
+          // },
+          // {
+          //   src: "/icon-512x512.png",
+          //   sizes: "512x512",
+          //   type: "image/png",
+          // },
+          // After using @vite-pwa/assets-generator
           {
-            src: "/icon-192x192.png",
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
-          // {
-          //   src: "/icon-256x256.png",
-          //   sizes: "256x256",
-          //   type: "image/png",
-          // },
           {
-            src: "/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
